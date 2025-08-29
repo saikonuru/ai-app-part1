c926d94eb9dedbc21a4e75665bf6c84f1fada1b9
saikonuru <sairam.konuru@gmail.com>
2025-08-29 21:27:24 +0530

    Connect FE and BE

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -9,6 +9,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send(`Hello AI App!  ${process.env.OPEN_API_KEY}`);
 });
 
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello Sai" });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
