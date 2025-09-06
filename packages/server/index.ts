cf46b74e5052b2f4f8e8179edbf3db603405a71c
saikonuru <sairam.konuru@gmail.com>
2025-09-06 18:11:56 +0530

    Add Chat Controller

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,48 +1,13 @@
 import dotenv from 'dotenv';
-import type { Request, Response } from 'express';
 import express from 'express';
-import { z } from 'zod';
-import { LLMError } from './llm-error';
-import { chatSchema } from './repository/chat.schema';
-import { chatService } from './services/chat.service';
+import { charController } from './controllers/chat.controller';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-app.post('/api/chat', async (req: Request, res: Response) => {
-   const parseResult = chatSchema.safeParse(req.body);
-
-   if (!parseResult.success) {
-      res.status(400).json(z.treeifyError(parseResult.error));
-      return;
-   }
-
-   const { prompt, conversationId } = parseResult.data;
-
-   try {
-      const llmResponse = await chatService.sendMessage(prompt, conversationId);
-
-      res.json({
-         message: llmResponse.message,
-         conversationId: conversationId,
-      });
-   } catch (error) {
-      console.error('Error calling LLM:', error);
-      if (error instanceof LLMError) {
-         return res.status(error.status).json({
-            message: error.message,
-            type: error.type,
-         });
-      }
-      // Fallback for any other unexpected errors
-      return res.status(500).json({
-         message:
-            'An unexpected error occurred while communicating with the assistant.',
-      });
-   }
-});
+app.post('/api/chat', charController.sendMessage);
 
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
