b28edd527c267113097dcf7d3f13a532a6e8cb2a
saikonuru <sairam.konuru@gmail.com>
2025-10-18 23:15:31 +0530

    Router changes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -5,7 +5,6 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-// app.use(router);
 app.use('/api', router);
 // const port = process.env.PORT || 3000;
 // if (process.env.NODE_ENV !== 'production') {
