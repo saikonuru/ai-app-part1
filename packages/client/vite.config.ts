87f0a1aef10111348d7642257bce7c3e8b8ff6e6
saikonuru <sairam.konuru@gmail.com>
2025-10-18 22:37:09 +0530

    Update api url

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      // '/api': 'http://localhost:3000',
+      '/api': 'https://ai-app-server-ashen.vercel.app',
     },
   },
   build: {outDir: 'dist'},
